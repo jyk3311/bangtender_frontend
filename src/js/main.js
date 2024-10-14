38adb916261d201dd677ec45ccb5ffa7b8a54ebb
rabongee <rmarkdehdals@gmail.com>
2024-10-14 21:44:06 +0900

    pull request merge

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,5 @@
 import { navbar } from './navbar.js';
 
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', function () {
-    const authLink = document.getElementById('auth-link');
-    const authButton = document.getElementById('auth-button');
-    const logoutButton = document.getElementById('logout-button');
-
-    const token = localStorage.getItem('access_token');
-    if (token) {
-        try {
-            // 유저가 인증된 경우
-            authLink.href = `pages/profile.html`;  // 프로필 페이지로 이동
-            authButton.textContent = '내 프로필';
-            logoutButton.style.display = 'inline';  // 로그아웃 버튼 보이기
-
-            // 로그아웃 버튼 클릭 시 이벤트
-            logoutButton.addEventListener('click', function () {
-                logoutUser(token);
-            });
-        } catch (error) {
-            console.error('토큰 디코딩 중 오류가 발생했습니다:', error);
-            localStorage.removeItem('access_token');  // 오류가 있을 경우 토큰 삭제
-=======
 document.addEventListener('DOMContentLoaded', async () => {
     navbar();
 
@@ -34,7 +12,6 @@ document.addEventListener('DOMContentLoaded', async () => {
         };
         if (token) {
             headers['Authorization'] = `Bearer ${token}`;
->>>>>>> dev
         }
 
         const response = await fetch('http://localhost:8000/api/v1/subcontents/', {
