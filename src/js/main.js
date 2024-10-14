08a148c759c1390cea1eb618d7ef7ffee59c31d9
rabongee <rmarkdehdals@gmail.com>
2024-10-14 22:06:04 +0900

    fix: pages navbar url변경
    - pages안의 html파일들의 navbar 분류별 주류 목록 불러오는 url 수정

--- src/js/main.js
+++ src/js/main.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             headers['Authorization'] = `Bearer ${token}`;
         }
 
-        const response = await fetch('http://localhost:8000/api/v1/subcontents/', {
+        const response = await fetch('http://43.203.219.114/api/v1/subcontents/', {
             method: 'GET',
             headers: headers,
         });
